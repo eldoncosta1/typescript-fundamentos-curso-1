0f9b77fdc6fa4924f08215ab972f018d350d994d
Eldon de Aquino Costa <eldon.costa@compasso.com.br>
2022-01-07 02:42:13 -0300

    Criação de decorators para aplicação

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,25 +1,24 @@
+import { logarTempoExecucao } from "../decorators/logar-tempo-de-execucao.js";
+import { inspect } from "../decorators/inspect.js";
+
 export abstract class View<T> {
 
   protected elemento: HTMLElement;
   private escapar = false;
 
-  constructor(seletor: string, escapar?: boolean) {
+  constructor(seletor: string) {
     const elemento = document.querySelector(seletor);
     if (elemento) {
       this.elemento = elemento as HTMLElement;
     } else {
       throw Error('Verifique se o elemento existe na DOM');
     }
-    if (escapar) {
-      this.escapar = escapar;
-    }
   }
 
+  @inspect()
+  @logarTempoExecucao(true)
   public update(model: T): void {
     let template = this.template(model);
-    if (this.escapar) {
-      template = template.replace(/<script>[\s\S]*?<\/scrpt>/, '');
-    }
     this.elemento.innerHTML = this.template(model);
   }
 
